977a22719b53e48a335f380462d10f3dfa7381f5
nebraswarsi <70344929+nebraswarsi@users.noreply.github.com>
2022-02-24 11:42:30 -0500

    Update cnn_explain.py

--- cnn_explain.py
+++ cnn_explain.py
@@ -41,8 +41,7 @@ try:
 except:
     # local debugging parameters
 
-    patients = ['SEEG-SK-02', 'SEEG-SK-04','SEEG-SK-06','SEEG-SK-07','SEEG-SK-09','SEEG-SK-11','SEEG-SK-14'
-    'SEEG-SK-15', 'SEEG-SK-18','SEEG-SK-19', 'SEEG-SK-20', 'SEEG-SK-21', 'SEEG-SK-24', 'SEEG-SK-26']
+    patients = []
 
 # Training Parameters
 epochs = 200
